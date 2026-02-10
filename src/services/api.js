e19e4ed8128a317352edf9a2d2eb23c9bc42735b
sopheak <sansopheak686@gmail.com>
2026-02-10 18:30:52 +0700

    add .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-learning-api-production-a6d4.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
   headers: {
     "Content-Type": "application/json",
   },
