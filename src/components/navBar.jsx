b0ccdfe81b52631ba2366b12846be6586336cd15
sopheak <sansopheak686@gmail.com>
2026-02-11 19:28:29 +0700

    rename image

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import Logo from "../assets/imgs/Academic hub.png";
+import Logo from "../assets/imgs/Academic-hub.png";
 export default function Navbar() {
   const token = localStorage.getItem("token");
   const role = localStorage.getItem("role");
